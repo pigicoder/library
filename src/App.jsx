f9db280ef3dcdb710a4ff38baeeba606d6740041
PierGiorgio <piergiorgio.gariboldi@objectmethod.it>
2026-09-25 12:47:41 +0200

    exercise library app

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Library system</h1>
+        </div>
+        <div class="card">
+          <h4><b>John Doe</b></h4>
+          <p>Architect & Engineer</p>
+          <div class="container"></div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
